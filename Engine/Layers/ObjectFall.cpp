3dee3b76193222d4f4d025d58439f0a32decd65d
JuliusWinther <mattiamcroll@gmail.com>
2025-05-21 15:14:37 +0200

    test

--- Engine/Layers/ObjectFall.cpp
+++ Engine/Layers/ObjectFall.cpp
@@ -70,7 +70,13 @@ void ObjectFallLayer::setAmount(uint32_t dropNum) {
 	for (uint32_t i = 0; i < dropNum; i++) dropSpawnOrder.emplace_back(i);
 	std::random_device rng;
 	std::default_random_engine urng(rng());
-	std::shuffle(dropSpawnOrder.begin(), dropSpawnOrder.end(), urng);
+
+	// W_UBUNTU-FIX
+	// std::shuffle(dropSpawnOrder.begin(), dropSpawnOrder.end(), urng);
+
+	static thread_local std::mt19937_64 rng{std::random_device{}()};
+	std::shuffle(objects.begin(), objects.end(), rng);
+	// END
 }
 
 void ObjectFallLayer::setWind(int32_t factor) {
@@ -80,7 +86,7 @@ void ObjectFallLayer::setWind(int32_t factor) {
 	//    ___//__                                 | /\φ|           the drops fall from top to bottom ("fall axis")
 	//   |  //   |               -->         left ./||\. right     exactly parallel to left and right
 	//   .__/θ)__|                                |\vv/|           and between the bounds left and right ("sky axis")
-	//top^  |/                                    |_\/_|
+	// top^  |/                                    |_\/_|
 	//   =(0,1080)                                  bottom         φ = 135° % 90 = 45°
 
 	MathVector<float> corners[]{MathVector<float>(0, 0),
@@ -133,7 +139,7 @@ void ObjectFallLayer::setBaseDrop(GPU_Image *newBaseDrop) {
 
 void ObjectFallLayer::setBaseDrop(SDL_Color &colour, uint32_t w, uint32_t h) {
 	drops.clear();
-	//TODO: add some gradients?
+	// TODO: add some gradients?
 	if (baseDrop->w != w || baseDrop->h != h) {
 		gpu.freeImage(baseDrop);
 		baseDrop = gpu.createImage(baseDropWidth, baseDropHeight, 4);
@@ -260,7 +266,7 @@ void ObjectFallLayer::refresh(GPU_Target *target, GPU_Rect &clip, float x, float
 }
 
 void ObjectFallLayer::commit() {
-	//sendToLog(LogLevel::Info, "Time to commit ObjectFallLayer\n");
+	// sendToLog(LogLevel::Info, "Time to commit ObjectFallLayer\n");
 	if (paused[CurrentScene] != paused[FormerScene]) {
 		old_drops.unset();
 		paused[FormerScene] = paused[CurrentScene];
